7ce6cf929da9a17d73a40699f24ef36d85dd1cee
larifsantos685 <larissafsantos.685@gmail.com>
2025-05-04 19:42:16 -0300

    Primeiro commit

---
+++ projetoagendamento/src/main/java/com/projetoagendamento/projetoagendamento/ProjetoagendamentoApplication.java
@@ -0,0 +1,13 @@
+package com.projetoagendamento.projetoagendamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoagendamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoagendamentoApplication.class, args);
+	}
+
+}
---
+++ projetoagendamento/src/test/java/com/projetoagendamento/projetoagendamento/ProjetoagendamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoagendamento.projetoagendamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoagendamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
